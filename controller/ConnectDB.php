61281f21329f568082f1f748a1021b2b8e2186f3
Kenneth Larsen <k3nny555@gmail.com>
2012-03-20 11:17:19 +0100

    Hurtigt fix til connectDB fejl besked

--- controller/ConnectDB.php
+++ controller/ConnectDB.php
@@ -2,11 +2,11 @@
 $conn = mysql_connect("localhost","root","");
 if (!$conn)
 	{
-	exit("Kunne ikke tilg&aring; databasen!");
+	exit("Kunne ikke tilg&aring; databasen.<br /> Prøv venligst igen.");
 	}
 $dbSelect = mysql_select_db("Kreatif");
 if (!$dbSelect)
 	{
-	exit("Kunne ikke tilg&aring; databasen!");
+	exit("Kunne ikke tilg&aring; databasen.<br /> Prøv venligst igen.");
 	}
 ?>
