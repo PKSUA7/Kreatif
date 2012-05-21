d61f0a8ebbd810cc979415ac63d46f49df8eafe8
Kenneth Larsen <k3nny555@gmail.com>
2012-05-21 18:10:56 +0200

    Link fix i menuen

--- view/SideBarView.php
+++ view/SideBarView.php
@@ -12,7 +12,7 @@
 		<a href="auctionoverview.php">Auktionsoversigt</a><br />
 		<a href="newauction.php">Opret auktion</a><br />
 		<a href="newartist.php">Opret kunster profil</a><br />
-		<a href="controller/update.php?redirect=true">Opdater system</a><br />
+		<a href="controller/Update.php?redirect=true">Opdater system</a><br />
 		<?php
 		}?>
 </div>
